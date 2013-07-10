d261fa3f9f6ddbf26d4ab1157a40196d85c24692
brito <francisco.brito@gmail.com>
2013-07-09 22:42:26 -0700

    Create $.fn.keytar.js

---
+++ $.fn.keytar.js
@@ -0,0 +1,7 @@
+/** keytarjs.org 2013 (cc) by-sa  */
+$.fn.keytar = function(keyCode, method){
+  $(document).keyup(function(e){
+    if (e.keyCode == keyCode)
+      $(this)[method]();
+  }.bind(this));
+};
